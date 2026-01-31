3bb19f402dc76eac49a4b1227789fdea88eaa587
Aleksa <office.aleksa.milanovic@gmail.com>
2026-01-31 15:42:33 +0100

    Connected to database

---
+++ database/database.php
@@ -0,0 +1,18 @@
+<?php
+
+$json_env = file_get_contents('././env.json');
+$env = json_decode($json_env);
+
+$servername = $env->servername;
+$username = $env->username;
+$password = $env->password;
+$dbname = $env->dbname;
+
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$db) {
+    exit('Connection to database failed.');
+
+} else {
+    echo 'Connected to database...';
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
 require_once './templates/header.php';
+require_once './database/database.php';
 require_once './router/router.php';
 require_once './templates/footer.php';
