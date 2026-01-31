932ff3fbdd6b9602bfb6cb617557b7e1909370ac
Aleksa <office.aleksa.milanovic@gmail.com>
2026-01-31 16:15:07 +0100

    Login

--- modules/login.php
+++ modules/login.php
@@ -1,5 +1,22 @@
-<?php?>
+<?php
+
+$phone_number_error = false;
+$pin_error = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $phone_number = htmlspecialchars(trim($_POST['phone-number']));
+    $pin = htmlspecialchars(trim($_POST['pin']));
+}
+
+?>
 
 <div class="login">
     <h2>Login</h2>
+
+    <form method="POST">
+        <input type="text" name="phone-number" placeholder="Phone number"><br><br>
+        <input type="password" name="pin" maxlength="4" placeholder="PIN"><br><br>
+        <button type="submit">Login</button>
+    </form>
 </div>
