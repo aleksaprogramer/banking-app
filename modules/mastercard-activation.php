e15bb6303bfeb2d2764bdec05221900b5d3cdac0
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-03 18:33:08 +0100

    Working on security

--- modules/mastercard-activation.php
+++ modules/mastercard-activation.php
@@ -5,6 +5,18 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
+// Validating user
+$current_user_id = $_SESSION['user_id'];
+
+$sql = "SELECT is_admin FROM accounts WHERE id = $current_user_id";
+$run = $db->query($sql);
+$results = $run->fetch_assoc();
+
+if ($results['is_admin'] === '1') {
+    header("Location: http://localhost/projects/banking-app/?router=register");
+    exit();
+}
+
 // Validating mastercard
 $current_user_id = $_SESSION['user_id'];
 
